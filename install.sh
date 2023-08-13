f199e0cf07324d63438d6b1bde710313ef4990fd
Hikmet Altıntaş <hikmet1517@gmail.com>
2023-08-13 14:02:30 +0300

    fix symlink creation

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 install -D tagbrowser.desktop ~/.local/share/applications/tagbrowser.desktop
 install -D tagbrowser.png ~/.local/share/icons/hicolor/512x512/apps/tagbrowser.png
-ln -f -s ./build/tagbrowser ~/.local/bin/tagbrowser
+ln -f -s "$(realpath ./build/tagbrowser)" ~/.local/bin/tagbrowser
 
 gtk-update-icon-cache -f -t ~/.local/share/icons/hicolor/
